bebc5ab4d92d6d66961d4e950a7c167a7b6d7ef7
mariapenaa <63109876+mariapenaa@users.noreply.github.com>
2022-02-23 00:15:32 -0300

    fix

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-require('dotenv').config({path:'../.env'})
+require('dotenv').config({path:'../../.env'})
 const serverless = require('serverless-http');
-app.use(cors())
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(cors())
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
-app.use(cors());
+
 app.use(express.json());
 app.use("/", router);
 
@@ -23,47 +23,27 @@ const contactEmail = nodemailer.createTransport({
       pass:process.env.REACT_APP_MAIL_PASSWORD,
     },
   });
-
+/* 
 contactEmail.verify(function(error, success) {
 if (error) {
     console.log(error);
 } else {
     console.log("Server is ready to take our messages");
 }
-});
+}); */
   
-  contactEmail.verify((error) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Ready to Send");
-    }
-  });
-
-  router.get('/', cors(), (req, res) => {
-    res.json(
-        [
-            {
-                'id': '001',
-                'name': 'Smith',
-                'email': 'smith@gmail.com'
-            },
-            {
-                'id': '002',
-                'name': 'Sam',
-                'email': 'sam@gmail.com'
-            },
-            {
-                'id': '003',
-                'name': 'lily',
-                'email': 'lily@gmail.com'
-            }
-        ]
-    )
-})
 
 
-  router.post("/contacto", cors(), (req, res) => {
+
+
+  router.post("/contacto", (req, res) => {
+    contactEmail.verify((error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Ready to Send");
+      }
+    });
     const name = req.body.name;
     const email = req.body.email;
     const subject = req.body.subject;
@@ -78,10 +58,12 @@ if (error) {
     contactEmail.sendMail(mail, (error) => {
       if (error) {
         res.json({ status: "failed" });
+        
       } else {
         res.json({ status: "sent" });
       }
     });
   });
 
+  module.exports = app;
   module.exports.handler = serverless(app);
