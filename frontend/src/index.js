f674c619e47238bb2522bbb10373d5365a2dd35e
mariapenaa <63109876+mariapenaa@users.noreply.github.com>
2023-02-07 11:35:58 -0300

    analytics fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import ReactGA from 'react-ga';
 
+
+const TRACKING_ID = "G-ELGJ0FNYFX"
+ReactGA.initialize(TRACKING_ID);
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
